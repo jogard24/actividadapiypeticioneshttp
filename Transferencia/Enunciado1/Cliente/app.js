707f19e57736a3623fdb673a01380e9b3b11d843
jogard24 <jroadiaz1@gmail.com>
2026-04-20 15:49:08 -0500

    Delete Transferencia directory fallo

--- Transferencia/Enunciado1/Cliente/app.js
+++
@@ -1,6 +0,0 @@
-import { resolverEjercicio1 } from "./Peticiones/index.js";
-
-const reporte = await resolverEjercicio1();
-
-console.log("--- Reporte de Usuarios Activos ---");
-console.log(reporte);
--- Transferencia/Enunciado3/Cliente/app.js
+++
@@ -1,7 +0,0 @@
-import { buscarDetallePublicacion } from "./Peticiones/index.js";
-
-let idPost = parseInt(prompt("Ingrese el ID de la publicación que desea consultar:"));
-const detalle = await buscarDetallePublicacion(idPost);
-
-console.log("--- Detalle de Búsqueda Específica ---");
-console.log(detalle);
