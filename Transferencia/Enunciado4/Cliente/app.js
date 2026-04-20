a94e2584ab3afaa35c218b555eaee0e9d7886317
jogard24 <jroadiaz1@gmail.com>
2026-04-20 15:56:29 -0500

    Delete Transferencia/Enunciado4 directory Revisa

--- Transferencia/Enunciado4/Cliente/app.js
+++
@@ -1,6 +0,0 @@
-import { eliminarPostSeguro } from "./Peticiones/index.js";
-
-let idAEliminar = prompt("Ingrese el ID de la publicación que desea eliminar:");
-const mensajeResultado = await eliminarPostSeguro(idAEliminar);
-
-console.log(mensajeResultado);
